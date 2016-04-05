15b9bf1201b15d5c16c5f3db24b20b7aaf69b90f
zeneke <zeneke@gmail.com>
2016-04-05 12:34:33 +0200

    added camera following the player

--- main.lua
+++ main.lua
@@ -1,11 +1,56 @@
 -- Fer's Challenge
 
+camera = {}
+camera.x = 0
+camera.y = 0
+camera.scaleX = 1
+camera.scaleY = 1
+camera.rotation = 0
+
+function camera:set()
+  love.graphics.push()
+  love.graphics.rotate(-self.rotation)
+  love.graphics.scale(1 / self.scaleX, 1 / self.scaleY)
+  love.graphics.translate(-self.x, -self.y)
+end
+
+function camera:unset()
+  love.graphics.pop()
+end
+
+function camera:move(dx, dy)
+  self.x = self.x + (dx or 0)
+  self.y = self.y + (dy or 0)
+end
+
+function camera:rotate(dr)
+  self.rotation = self.rotation + dr
+end
+
+function camera:scale(sx, sy)
+  sx = sx or 1
+  self.scaleX = self.scaleX * sx
+  self.scaleY = self.scaleY * (sy or sx)
+end
+
+function camera:setPosition(x, y)
+  self.x = x or self.x
+  self.y = y or self.y
+end
+
+function camera:setScale(sx, sy)
+  self.scaleX = sx or self.scaleX
+  self.scaleY = sy or self.scaleY
+end
+
+----
+
 SCREEN_WIDTH = 8
 SCREEN_HEIGHT = 8
 
 PIXEL_WIDTH = love.graphics.getWidth() / SCREEN_WIDTH
 PIXEL_HEIGHT = love.graphics.getHeight() / SCREEN_HEIGHT
-MAX_STARS = PIXEL_WIDTH * PIXEL_HEIGHT
+
 
 MAP = {}
 
@@ -24,6 +69,7 @@ function love.load(arg)
 end
 
 function love.update(dt)
+    
 
 	if love.keyboard.isDown('escape') then
 		love.event.push('quit')
@@ -68,24 +114,28 @@ function love.update(dt)
 		
 		if player.direction == 'left' and player.screen_x >= player.dst_x then
 			player.screen_x = player.screen_x - (player.speed * dt)
+            camera.x = player.screen_x - (player.speed * dt)
 			if player.screen_x <= player.dst_x then
 				player.x = player.x - 1
 				player.moving = false
 			end
         elseif player.direction == 'right' and player.screen_x <= player.dst_x then
 			player.screen_x = player.screen_x + (player.speed * dt)
+            camera.x = player.screen_x + (player.speed * dt)
 			if player.screen_x >= player.dst_x then
-				player.x = player.x + 1
+				player.x = player.x + 1                
 				player.moving = false
 			end
         elseif player.direction == 'up' and player.screen_y >= player.dst_y then
 			player.screen_y = player.screen_y - (player.speed * dt)
+            camera.y = player.screen_y - (player.speed * dt)
 			if player.screen_y <= player.dst_y then
 				player.y = player.y - 1
 				player.moving = false
 			end
         elseif player.direction == 'down' and player.screen_y <= player.dst_y then
 			player.screen_y = player.screen_y + (player.speed * dt)
+            camera.y = player.screen_y + (player.speed * dt)
 			if player.screen_y >= player.dst_y then
 				player.y = player.y + 1
 				player.moving = false
@@ -96,6 +146,7 @@ function love.update(dt)
 end
 
 function love.draw(dt)
+    camera:set()
 
 	for j=0,SCREEN_HEIGHT do
         for i=0,SCREEN_WIDTH do
@@ -108,5 +159,5 @@ function love.draw(dt)
 
     love.graphics.setColor(0, 0, 255, 100)
     love.graphics.rectangle('fill',player.screen_x, player.screen_y, PIXEL_WIDTH,PIXEL_HEIGHT)
-	
+	camera:unset()
 end
